e8bca58e5c9885e1b2ab6f1807c21cd2fb417daf
Slava Kachkanov <slava.kachkanov@ukaea.uk>
2025-10-31 12:08:04 +0000

    Add initial test cases for daily_max() and daily_min()

--- inflammation/models.py
+++ inflammation/models.py
@@ -30,8 +30,7 @@ def daily_mean(data):
 def daily_max(data):
     """Calculate the daily maximum of a 2D inflammation data array for each day.
 
-   :param data: A 2D data array with inflammation data 
-   (each row contains measurements for a single patient across all days).
+   :param data: A 2D data array with inflammation data(each row contains measurements for a single patient across all days).
    :returns: An array of max values of measurements for each day.
    """
     return np.max(data, axis=0)
